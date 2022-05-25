6ef49663676d3a0719afd5a936a81a945784805c
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2022-05-25 20:04:36 +0200

    update

--- src/app/servieces/selected-item.service.spec.ts
+++ src/app/servieces/selected-item.service.spec.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../models/recipe.interface';
 import { TestBed } from '@angular/core/testing';
 
 import { SelectedItemService } from './selected-item.service';
@@ -12,5 +13,44 @@ describe('SelectedItemService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should set recipes', () => {
+    const recipes: Recipe[] = [
+      {
+        _id: '1',
+        name: '1',
+        description: '1',
+        preparationTimeInMinutes: 10,
+        ingredients: [{ _id: '1', name: '1', quantity: '1' }],
+      },
+      {
+        _id: '2',
+        name: '2',
+        description: '1',
+        preparationTimeInMinutes: 11,
+        ingredients: [{ _id: '2', name: '1', quantity: '1' }],
+      },
+    ];
+    service.recipes = recipes;
+    expect(service.recipes).toEqual(recipes);
+  });
+
+  it('should get refhreshRecipes', () => {
+    const refhresh = true;
+    service.refhreshRecipes = true;
+    expect(service.refhreshRecipes).toEqual(refhresh);
+  });
+
+  it('should set added', () => {
+    const added = true;
+    service.added = true;
+    expect(service.added).toEqual(added);
+  });
+
+  it('should get edittingMode', () => {
+    const edittingMode = true;
+    service.edittingMode = true;
+    expect(service.edittingMode).toEqual(edittingMode);
   });
 });
